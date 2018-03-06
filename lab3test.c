5070c72372d4e21c23374b8adc327d059fe262a2
Daniel Stinson-Diess <dstin002@ucr.edu>
2018-03-06 13:07:23 -0800

    Add initial implementation of shm_open(..), only includes case 1

--- lab3test.c
+++ lab3test.c
@@ -6,7 +6,7 @@ int fib(int);
 
 int main(int argc, char** argv) {
 
-    fib(100);
+    fib(200);
 
     exit();
     return 0;
@@ -16,16 +16,8 @@ int fib(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
 
+
     return fib(n-1) + fib(n-2);
 }
 
-unsigned long returnZero(unsigned long num) {
-    int arr[1000];
 
-    arr[0] = 0;
-
-    if (num == arr[0])
-        return 0;
-    
-    return returnZero(num - 1);
-}
